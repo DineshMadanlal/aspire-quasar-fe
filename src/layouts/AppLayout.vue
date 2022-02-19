8b0182c134242e674c2373f1257179de69295993
Dinesh Madanlal <>
2022-02-19 14:48:59 +0530

    add font family and sidebar route design

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,35 +1,100 @@
 <template>
-  <q-layout view="lhh lpR lFf">
-
-    <q-header class="bg-primary text-white">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left" />
-
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
-        </q-toolbar-title>
+  <q-layout view="Lhh lpR lFf" class="app-layout">
+    <!-- Header -->
+    <q-header class="app-layout-header">
+      <q-toolbar class="app-header-toolbar">
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left">
-      <!-- drawer content -->
+    <!-- Drawer/Sidebar -->
+    <q-drawer
+      dark
+      :width="340"
+      show-if-above
+      v-model="sidebarOpen"
+
+      side="left"
+      class="app-layout-sidebar"
+    >
+      <router-link
+        to="/cards"
+        class="router-home-link"
+      >
+        <img
+          src="assets/app-full-logo.svg"
+          svg-inline
+          alt="aspire logo"
+          class="no-outline"
+        />
+      </router-link>
+      <p class="app-desc-text">
+        Trusted way of banking for 3,000+ SMEs and startups in Singapore
+      </p>
+
+      <!-- routes grid -->
+      <div class="app-routes-grid">
+        <q-item
+          v-for="(feature, index) in appRoutes"
+          :key="`app-routes-${index}-${feature.label}`"
+
+          class="flex items-center no-wrap app-project-route"
+          :class="{ active: activeRoutePath.includes(feature.route) }"
+          :to="feature.route"
+
+          clickable
+          v-ripple
+        >
+          <!-- home icon -->
+          <img
+            v-if="feature.icon === 'home'"
+            src="assets/icons/home.svg"
+            svg-inline
+            class="no-outline"
+          />
+          <!-- card icon -->
+          <img
+            v-if="feature.icon === 'card'"
+            src="assets/icons/card.svg"
+            svg-inline
+            class="no-outline"
+          />
+          <!-- credit icon -->
+          <img
+            v-if="feature.icon === 'credit'"
+            src="assets/icons/credit.svg"
+            svg-inline
+            class="no-outline"
+          />
+          <!-- payments icon -->
+          <img
+            v-if="feature.icon === 'payments'"
+            src="assets/icons/payments.svg"
+            svg-inline
+            class="no-outline"
+          />
+          <!-- settings icon -->
+          <img
+            v-if="feature.icon === 'settings'"
+            src="assets/icons/settings.svg"
+            svg-inline
+            alt="settings"
+            class="no-outline"
+          />
+          <p class="feature-label-text">
+            {{ feature.label }}
+          </p>
+        </q-item>
+      </div>
     </q-drawer>
 
+    <!-- Page Container/Content -->
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
-        </q-toolbar-title>
+    <!-- Footer -->
+    <q-footer class="app-layout-footer">
+      <q-toolbar class="app-footer-toolbar">
       </q-toolbar>
     </q-footer>
 
@@ -41,12 +106,95 @@ export default {
   name: 'AppLayout',
   data() {
     return {
-      left: false,
+      sidebarOpen: false,
     };
+  },
+  computed: {
+    activeRoutePath() {
+      return this.$route.path;
+    },
+    // ----- STATIC DATA BEGIN ----
+    appRoutes() {
+      return [
+        {
+          icon: 'home',
+          label: 'Home',
+          route: '/home',
+        },
+        {
+          icon: 'card',
+          label: 'Cards',
+          route: '/cards',
+        },
+        {
+          icon: 'payments',
+          label: 'Payments',
+          route: '/payments',
+        },
+        {
+          icon: 'credit',
+          label: 'Credit',
+          route: '/credit',
+        },
+        {
+          icon: 'settings',
+          label: 'Settings',
+          route: '/settings',
+        },
+      ];
+    },
+    // ----- STATIC DATA END -------
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.app-layout {
+  /** Header and footer visible only for mobile devices. */
+  .app-layout-header, .app-layout-footer {
+    display: none;
+    @media (max-width: $breakpoint-xs-max) {
+      display: flex;
+    }
+  }
+}
+</style>
 
+<style lang="scss">
+/** The sidebar CSS should not be scoped for it to reflect in the browser */
+.app-layout {
+  .app-layout-sidebar {
+    .q-drawer--left {
+      padding: 48px;
+
+      .router-home-link {
+        display: block;
+        line-height: 0;
+      }
+
+      .app-desc-text {
+        color: rgba($color: $white, $alpha: 0.3);
+        font-size: 14px;
+        line-height: 20px;
+        margin-top: 19px;
+      }
+
+      .app-routes-grid {
+        display: grid;
+        grid-row-gap: 40px;
+        margin-top: 61px;
+
+        .app-project-route {
+          border-radius: 4px;
+          .feature-label-text {
+            font-size: 16px;
+            line-height: 32px;
+            color: $white;
+            margin-left: 16px;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
