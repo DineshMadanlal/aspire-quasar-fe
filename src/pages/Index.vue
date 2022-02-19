3aa7f8db92babd87e3fa53077ea7e4fcaa03e8d5
Dinesh Madanlal <>
2022-02-19 11:57:04 +0530

    app assets

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,5 +6,33 @@
 <script>
 export default {
   name: 'PageIndex',
+  // Meta tags for SEO purpose
+  meta() {
+    return {
+      title: 'Aspire',
+      meta: {
+        title: {
+          name: 'title',
+          content: 'Aspire',
+        },
+        ogTitle: {
+          name: 'og:title',
+          content: 'All-in-one Finance For Growing Businesses',
+        },
+        twitterTitle: {
+          name: 'twitter:title',
+          content: 'All-in-one Finance For Growing Businesses',
+        },
+        description: {
+          name: 'description',
+          content: 'Made For startups and SMEs. Save time and money with business accounts, borderless payments, corporate cards, expense management, all in one.',
+        },
+        twitterDescription: {
+          name: 'twitter:description',
+          content: 'Made For startups and SMEs. Save time and money with business accounts, borderless payments, corporate cards, expense management, all in one.',
+        },
+      },
+    };
+  },
 };
 </script>
